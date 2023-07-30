794e0f88129d1740b3020e53c1cb2bf9ec182da7
Kayki Letieri <kaykiletieri37@gmail.com>
2023-07-30 20:38:24 -0300

    feat(merge): fix merge

--- Models/Period.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace adstaskhub_api.Models
-{
-    public class Period
-    {
-        public virtual long Id { get; set; }
-        public virtual int Number { get; set; }
-    }
-}
--- Models/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace adstaskhub_api.Models
-{
-    public class Role
-    {
-        public virtual long Id { get; set; }
-        public virtual string Name { get; set; }
-    }
-}
